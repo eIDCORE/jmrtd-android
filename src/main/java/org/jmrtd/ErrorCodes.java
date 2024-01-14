04234c2f86c5ab3962c69c725485f7d60c9facaa
eIDCORE <eid.plaut@gmail.com>
2024-01-14 23:12:34 +0100

    Init commit

---
+++ src/main/java/org/jmrtd/ErrorCodes.java
@@ -0,0 +1,18 @@
+package org.jmrtd;
+
+public class ErrorCodes {
+    public static final int SW_NON_VOLATILE_MEMORY_CHANGED_COUNTER_8 = (short) 0x63C8;
+    public static final int SW_NON_VOLATILE_MEMORY_CHANGED_COUNTER_7 = (short) 0x63C7;
+    public static final int SW_NON_VOLATILE_MEMORY_CHANGED_COUNTER_6 = (short) 0x63C6;
+    public static final int SW_NON_VOLATILE_MEMORY_CHANGED_COUNTER_5 = (short) 0x63C5;
+    public static final int SW_NON_VOLATILE_MEMORY_CHANGED_COUNTER_4 = (short) 0x63C4;
+    public static final int SW_NON_VOLATILE_MEMORY_CHANGED_COUNTER_3 = (short) 0x63C3;
+    public static final int SW_NON_VOLATILE_MEMORY_CHANGED_COUNTER_2 = (short) 0x63C2;
+    public static final int SW_NON_VOLATILE_MEMORY_CHANGED_COUNTER_1 = (short) 0x63C1;
+    public static final int SW_NON_VOLATILE_MEMORY_CHANGED_COUNTER_0 = (short) 0x63C0;
+    public static final int CAN_INVALID = (short) 0x6300;
+    public static final int PIN_NOT_ACTIVATED = (short) 0x6A88;
+    public static final int PIN_NOT_ACTIVATED_2 = (short) 0x6984;
+    public static final int PIN_BLOCKED = (short) 0x6983;
+    public static final int PUK_NOT_PRESENT = (short) 0x6982;
+}
